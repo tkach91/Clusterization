161d5225a318e53a29af0f9135520743b2b13295
tkach91 <tkach91@gmail.com>
2013-08-05 13:27:54 +0400

    Source code

---
+++ lab04/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace lab04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnMake = new System.Windows.Forms.Button();
+            this.tbxEps = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxClusterCount = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClearPoints = new System.Windows.Forms.Button();
+            this.btnDelPoints = new System.Windows.Forms.Button();
+            this.lbxPoints = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pbxMain = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.btnMake);
+            this.panel1.Controls.Add(this.tbxEps);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.tbxClusterCount);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btnClearPoints);
+            this.panel1.Controls.Add(this.btnDelPoints);
+            this.panel1.Controls.Add(this.lbxPoints);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(483, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(216, 456);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnMake
+            // 
+            this.btnMake.Location = new System.Drawing.Point(8, 292);
+            this.btnMake.Name = "btnMake";
+            this.btnMake.Size = new System.Drawing.Size(200, 23);
+            this.btnMake.TabIndex = 8;
+            this.btnMake.Text = "Запилить";
+            this.btnMake.UseVisualStyleBackColor = true;
+            this.btnMake.Click += new System.EventHandler(this.btnMake_Click);
+            // 
+            // tbxEps
+            // 
+            this.tbxEps.Location = new System.Drawing.Point(133, 257);
+            this.tbxEps.Name = "tbxEps";
+            this.tbxEps.Size = new System.Drawing.Size(75, 20);
+            this.tbxEps.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 260);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Погрешность";
+            // 
+            // tbxClusterCount
+            // 
+            this.tbxClusterCount.Location = new System.Drawing.Point(133, 227);
+            this.tbxClusterCount.Name = "tbxClusterCount";
+            this.tbxClusterCount.Size = new System.Drawing.Size(75, 20);
+            this.tbxClusterCount.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 230);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Количество кластеров";
+            // 
+            // btnClearPoints
+            // 
+            this.btnClearPoints.Location = new System.Drawing.Point(110, 191);
+            this.btnClearPoints.Name = "btnClearPoints";
+            this.btnClearPoints.Size = new System.Drawing.Size(98, 23);
+            this.btnClearPoints.TabIndex = 3;
+            this.btnClearPoints.Text = "Очистить";
+            this.btnClearPoints.UseVisualStyleBackColor = true;
+            this.btnClearPoints.Click += new System.EventHandler(this.btnClearPoints_Click);
+            // 
+            // btnDelPoints
+            // 
+            this.btnDelPoints.Location = new System.Drawing.Point(6, 191);
+            this.btnDelPoints.Name = "btnDelPoints";
+            this.btnDelPoints.Size = new System.Drawing.Size(98, 23);
+            this.btnDelPoints.TabIndex = 2;
+            this.btnDelPoints.Text = "Удалить";
+            this.btnDelPoints.UseVisualStyleBackColor = true;
+            this.btnDelPoints.Click += new System.EventHandler(this.btnDelPoints_Click);
+            // 
+            // lbxPoints
+            // 
+            this.lbxPoints.FormattingEnabled = true;
+            this.lbxPoints.Location = new System.Drawing.Point(6, 25);
+            this.lbxPoints.Name = "lbxPoints";
+            this.lbxPoints.Size = new System.Drawing.Size(202, 160);
+            this.lbxPoints.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Точки";
+            // 
+            // pbxMain
+            // 
+            this.pbxMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbxMain.Location = new System.Drawing.Point(0, 0);
+            this.pbxMain.Name = "pbxMain";
+            this.pbxMain.Size = new System.Drawing.Size(477, 456);
+            this.pbxMain.TabIndex = 1;
+            this.pbxMain.TabStop = false;
+            this.pbxMain.SizeChanged += new System.EventHandler(this.pbxMain_SizeChanged);
+            this.pbxMain.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 456);
+            this.Controls.Add(this.pbxMain);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Лаболаторная работа №4. Метод К-средних";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMain)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox tbxClusterCount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClearPoints;
+        private System.Windows.Forms.Button btnDelPoints;
+        private System.Windows.Forms.ListBox lbxPoints;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pbxMain;
+        private System.Windows.Forms.Button btnMake;
+        private System.Windows.Forms.TextBox tbxEps;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ lab04/Form1.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace lab04
+{
+    public partial class Form1 : Form
+    {
+        private ArrayList Points;
+        private ArrayList Clusters;
+        private bool ClustersFound = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Points = new ArrayList();
+
+            pbxMain.Image = new Bitmap(pbxMain.Width, pbxMain.Height);
+            
+
+        }
+
+        private void ClearImage()
+        {
+            Graphics gpsMain = Graphics.FromImage(pbxMain.Image);
+
+            gpsMain.FillRectangle(Brushes.White, 0, 0, pbxMain.Width, pbxMain.Height);
+
+            gpsMain.Dispose();
+
+            pbxMain.Refresh();
+        }
+
+        private void DrawPoints(bool Clear = true)
+        {
+            if (Clear)
+                ClearImage();
+
+            Graphics gpsMain = Graphics.FromImage(pbxMain.Image);
+
+            for (int i = 0; i < Points.Count; i++)
+            {
+                Point Cur = (Point)Points[i];
+                gpsMain.DrawEllipse(Pens.Black, Cur.x - 2, Cur.y - 2, 4, 4);
+            }
+
+            gpsMain.Dispose();
+
+            pbxMain.Refresh();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Левая кнопка добавление
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                Point P = new Point();
+                P.x = e.Location.X;
+                P.y = e.Location.Y;
+                Points.Add(P);
+
+                StringBuilder SB = new StringBuilder();
+                SB.AppendFormat("({0};{1})", P.x, P.y);
+
+                lbxPoints.Items.Add(SB.ToString());
+
+                DrawPoints();
+                ClustersFound = false;
+            }
+            else
+            {
+                // Правая удаление
+                for (int i = 0; i < Points.Count; i++)
+                {
+                    Point Cur = (Point)Points[i];
+
+                    if ((Cur.x - e.Location.X) * (Cur.x - e.Location.X) +
+                            (Cur.y - e.Location.Y) * (Cur.y - e.Location.Y) < 25)
+                    {
+                        Points.RemoveAt(i);
+                        lbxPoints.Items.RemoveAt(i);
+
+                        DrawPoints();
+                        ClustersFound = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void btnDelPoints_Click(object sender, EventArgs e)
+        {
+            if (lbxPoints.SelectedIndex >= 0)
+            {
+                Points.RemoveAt(lbxPoints.SelectedIndex);
+                lbxPoints.Items.RemoveAt(lbxPoints.SelectedIndex);
+
+                DrawPoints();
+                ClustersFound = false;
+            }
+        }
+
+        private void btnClearPoints_Click(object sender, EventArgs e)
+        {
+            lbxPoints.Items.Clear();
+            ClearImage();
+            Points.Clear();
+            ClustersFound = false;
+        }
+
+        private double Length(int PointInd, int ClusterInd)
+        {
+            Point P = (Point)Points[PointInd];
+            Cluster C = (Cluster)Clusters[ClusterInd];
+
+            return (P.x - C.x) * (P.x - C.x) + (P.y - C.y) * (P.y - C.y);
+        }
+
+        private double Length(Point P, int ClusterInd)
+        {
+            Cluster C = (Cluster)Clusters[ClusterInd];
+
+            return (P.x - C.x) * (P.x - C.x) + (P.y - C.y) * (P.y - C.y);
+        }
+
+        private int FindClustNumber(Point P)
+        {
+            int MinInd = 0;
+            double MinLen = Length(P, 0);
+
+            for (int j = 0; j < Clusters.Count; j++)
+            {
+                double CurLen = Length(P, j);
+                if (MinLen > CurLen)
+                {
+                    MinInd = j;
+                    MinLen = CurLen;
+                }
+            }
+
+            return MinInd;
+        }
+
+        private void FindClusters()
+        {
+            for (int i = 0; i < Clusters.Count; i++)
+                ((Cluster)Clusters[i]).Points.Clear();
+
+            // Ищем к каким кластерам принадлежит каждая точка
+            for (int i = 0; i < Points.Count; i++)
+            {
+                int MinInd = 0;
+                double MinLen = Length(i, 0);
+
+                for (int j = 0; j < Clusters.Count; j++)
+                {
+                    double CurLen = Length(i, j);
+                    if (MinLen > CurLen)
+                    {
+                        MinInd = j;
+                        MinLen = CurLen;
+                    }
+                }
+
+                ((Cluster)Clusters[MinInd]).Points.Add(i);
+            }
+        }
+
+        private double CorrectClusters()
+        {
+            double e = 0;
+
+            for (int i = 0; i < Clusters.Count; i++)
+            {
+                double mean_x = 0;
+                double mean_y = 0;
+
+                Cluster CurCluster = (Cluster)Clusters[i];
+
+                if (CurCluster.Points.Count == 0)
+                    continue;
+
+                for (int j = 0; j < CurCluster.Points.Count; j++)
+                {
+                    Point CurPoint = (Point)Points[(int)CurCluster.Points[j]];
+                    mean_x += CurPoint.x;
+                    mean_y += CurPoint.y;
+                }
+
+                mean_x /= CurCluster.Points.Count;
+                mean_y /= CurCluster.Points.Count;
+
+                e += (mean_x - CurCluster.x) * (mean_x - CurCluster.x) + (mean_y - CurCluster.y) * (mean_y - CurCluster.y);
+
+                CurCluster.x = Convert.ToInt32(mean_x);
+                CurCluster.y = Convert.ToInt32(mean_y);
+            }
+
+            return e;
+        }
+
+        private void InitClusters(int count)
+        {
+            Clusters = new ArrayList();
+            Random r = new Random();
+
+            int MaxX = Int32.MinValue, MaxY = Int32.MinValue, MinX = Int32.MaxValue, MinY = Int32.MaxValue;
+            // Смотрим максимальные и минимальные координаты точек
+            for (int i = 0; i < Points.Count; i++)
+            {
+                Point Cur = (Point)Points[i];
+
+                if (Cur.x > MaxX)
+                    MaxX = Cur.x;
+                if (Cur.x < MinX)
+                    MinX = Cur.x;
+                if (Cur.y > MaxY)
+                    MaxY = Cur.y;
+                if (Cur.y < MinY)
+                    MinY = Cur.y;
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                Cluster Cur = new Cluster();
+                Cur.x = (int)Math.Round(r.NextDouble() * (MaxX - MinX) + MinX);
+                Cur.y = (int)Math.Round(r.NextDouble() * (MaxY - MinY) + MinY);
+                Cur.C = Color.FromArgb((int)((r.Next() % 0xFFFFFF) | 0xFF000000));
+
+                Clusters.Add(Cur);
+            }
+        }
+
+        private void DrawClusters()
+        {
+            Bitmap Bmp = (Bitmap)pbxMain.Image;
+            Graphics Gph = Graphics.FromImage(Bmp);
+
+            for (int i = 0; i < pbxMain.Width; i++)
+                for (int j = 0; j < pbxMain.Height; j++)
+                {
+                    Point P = new Point();
+                    P.x = i;
+                    P.y = j;
+                    int num = FindClustNumber(P);
+
+                    Bmp.SetPixel(i, j, ((Cluster)Clusters[num]).C); 
+                }
+
+            // Рисуем центры кластеро
+            for (int i = 0; i < Clusters.Count; i++)
+            {
+                Cluster Cur = (Cluster)Clusters[i];
+                Gph.DrawEllipse(new Pen(Color.Red), (float)Cur.x, (float)Cur.y, 2, 2); 
+            }
+
+            Brush BB = new SolidBrush(Color.Black);
+
+            DrawPoints(false);
+            pbxMain.Refresh();
+                
+        }
+
+        private void btnMake_Click(object sender, EventArgs e)
+        {
+            int cc = Convert.ToInt32(tbxClusterCount.Text);
+            double eps = Convert.ToDouble(tbxEps.Text);
+
+            InitClusters(cc);
+
+            double last_eps = 0, cur_eps = 0;
+
+            do
+            {
+                last_eps = cur_eps;
+                FindClusters();
+                cur_eps = CorrectClusters();
+            } while (Math.Abs(cur_eps - last_eps) > eps);
+
+            DrawClusters();
+            ClustersFound = true;
+        }
+
+        private void pbxMain_SizeChanged(object sender, EventArgs e)
+        {
+            if (pbxMain.Image != null)
+                pbxMain.Image.Dispose();
+
+            pbxMain.Image = new Bitmap(pbxMain.Width, pbxMain.Height);
+
+            if (ClustersFound)
+                DrawClusters();
+            else
+                DrawPoints();
+        }
+    }
+
+    public class Point
+    {
+        public int x, y;
+    }
+
+    public class Cluster
+    {
+        public int x, y;
+        public ArrayList Points;
+        public Color C;
+
+        public Cluster()
+        {
+            Points = new ArrayList(); 
+        }
+    }
+
+}
